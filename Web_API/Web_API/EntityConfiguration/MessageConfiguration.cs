45faac33a8eb548d8b15c69c9eb82b06755bc634
denys-u <denys.udovik@nure.ua>
2021-07-21 23:50:30 +0300

    added Entity Configs

---
+++ Web_API/Web_API/EntityConfiguration/MessageConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web_API.Entities;
+
+namespace Web_API.EntityConfiguration
+{
+    public class MessageConfiguration : IEntityTypeConfiguration<Message>
+    {
+        public void Configure(EntityTypeBuilder<Message> builder)
+        {
+            builder.ToTable("Message").HasKey(p => p.Id);
+            builder.Property(p => p.Id).HasColumnName("MessageId");
+            builder.Property(p => p.DateOfTransmission).IsRequired().HasColumnName("DateOfTransmission").HasColumnType("datetime2");
+        }
+    }
+}
---
+++ Web_API/Web_API/EntityConfiguration/UserConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web_API.Entities;
+
+namespace Web_API.EntityConfiguration
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("User").HasKey(p => p.Id);
+            builder.Property(p => p.Id).HasColumnName("serId");
+            builder.Property(p => p.Nickname).IsRequired().HasColumnName("Nickname").HasMaxLength(50);
+            builder.Property(p => p.RegestrationDate).IsRequired().HasColumnName("RegestrationDate").HasColumnType("datetime2");
+        }
+    }
+}
