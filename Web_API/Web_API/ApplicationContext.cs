2f0336b80df52cdbcd90d401e52536e4c530d7f6
denys-u <denys.udovik@nure.ua>
2021-07-22 00:37:09 +0300

    added Aplication Context

---
+++ Web_API/Web_API/ApplicationContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+using Web_API.Entities;
+using Web_API.EntityConfiguration;
+
+namespace Web_API
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext([NotNullAttribute] DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Message> Messages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AdminConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new MessageConfiguration());
+        }
+    }
+}
